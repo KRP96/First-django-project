227160196244c9a7b4cc56fe69509782f30e3366
Kavinda Rashmika Peiris <86865401+KRP96@users.noreply.github.com>
2021-07-31 11:40:53 +0530

    Delete My_First_Project directory

--- My_First_Project/meetups/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('meetups/', views.welcome),   #our-domain.com/meetups/
-    path('meetups/<slug:meetup_slug>', views.meetup_details),  #our-domain.com/meetups/<dynamic-path-segment>
-]
